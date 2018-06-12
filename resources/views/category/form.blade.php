9a153bba4a53dbbb576e621d7e886b8fae193677
huy0105 <huynv@nal.vn>
2018-06-12 14:19:12 +0700

    first commit

---
+++ resources/views/category/form.blade.php
@@ -0,0 +1,24 @@
+ <div class="font-group">
+                <label for="title">Work Name</label>
+                <input type="text" class="form-control" name="title" id="title"></input>
+            </div>
+            <div class="font-group">
+                <label for="des">Description</label>
+                <textarea name="description" id="description" cols="20" id="description" row="5" class="form-control"></textarea>
+            </div>
+            <div class="font-group">
+                <label for="title">Start Day</label>
+                <input type="date_format" name="startday" id="startday" class="form-control" autocomplete="off"></input>
+            </div>
+            <div class="font-group">
+                <label for="title">End Day</label>
+                <input type="date_format" class="form-control" id="endday" name="endday" autocomplete="off"></input>
+            </div>
+            <label for="title" id="status">Status</label>
+        <div class="col-xs-5">
+            <select class="form-control" id="status" name="status">
+                <option value="Planing">Planing</option>
+                <option value="Doing">Doing</option>
+                <option value="Complete">Complete</option>              
+            </select>
+        </div>
